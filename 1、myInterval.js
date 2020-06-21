3cb343d4fa42a4051c4997a50c31a338c09160c1
jingshuai <jingshuai@huobi.com>
2020-06-21 22:23:54 +0800

    bugfixed: myInterval

--- 1、myInterval.js
+++ 1、myInterval.js
@@ -7,13 +7,14 @@ const mySetInterval = (fn, a, b) => {
 		if(_idx < _interval.length) {
 			clearInterval(_currentInterval)
 			_currentInterval =  setInterval(() => {
-				_idx++
 				fn()
+				_idx++
 				_mySetInterval(_interval[_idx])
 			}, _t)
 			return _currentInterval
 		} else {
-			_idx = -1
+			_idx = 0
+			_mySetInterval(_interval[_idx])
 		}
 	}
 	_mySetInterval(_interval[_idx])
@@ -24,7 +25,10 @@ const myClear = (_interval) => {
 	clearInterval(_interval)
 }
 
-// let interval = mySetInterval(() => {
-// 	console.log('太难了')
-// }, 1000, 2000)
+console.time('太难了')
+let interval = mySetInterval(() => {
+	console.timeEnd('太难了')
+	console.time('太难了')
+}, 1000, 1000)
 
+// myClear(interval)
